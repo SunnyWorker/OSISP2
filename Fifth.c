d7371b49a2d03bb9627199a27e8242c080bb135e
SunnyWorker <93293748+SunnyWorker@users.noreply.github.com>
2022-03-04 15:04:15 +0300

    Update Fifth.c

--- Fifth.c
+++ Fifth.c
@@ -4,20 +4,20 @@
 
 void Read(const char *c) {
 
-DIR *dr = opendir(c);
-struct dirent *d;
-d = readdir(dr);
-while (d != NULL) {
-        printf("%s\n",d->d_name);
+        DIR *dr = opendir(c);
+        struct dirent *d;
         d = readdir(dr);
-}
+        while (d != NULL) {
+                printf("%s\n",d->d_name);
+                d = readdir(dr);
+        }
 
 }
 
 int main(int argc, char *argv[]) {
 
-Read("/");
-printf("Home page\n");
-Read("./");
+        Read("/");
+        printf("Home page\n");
+        Read("./");
 
 }
