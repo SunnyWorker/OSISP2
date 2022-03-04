55ed65e9ef1bbf9c2408afed180a87f3ec3cf34f
SunnyWorker <93293748+SunnyWorker@users.noreply.github.com>
2022-03-04 15:06:14 +0300

    Update Individ3.c

--- Individ3.c
+++ Individ3.c
@@ -6,59 +6,64 @@
 #include <string.h>
 
 void ReadCat(const char *ct, const char *con, int t1, int t2, int s1, int s2, FILE *file,int *a) {
-char s[200];
-char ss[200];
-strcpy(s,ct);
-strcpy(ss,ct);
-int size, time;
-struct dirent *d;
-struct stat buf;
-
-strcat(s,con);
-strcat(ss,con);
-
-if(strcmp(con,"")!=0) strcat(ss,"/");
-DIR *dr = opendir(ss);
-if(dr!=NULL) {
-	d = readdir(dr);
-	while (d != NULL && strcmp(d->d_name,".")!=0) {
-		(*a)++;
-		ReadCat(ss,d->d_name,t1,t2,s1,s2,file,a);
-		d = readdir(dr);
-	}
+	char s[200];
+	char ss[200];
+	int size, time;
 	
-
-}
-else {
+	strcpy(s,ct);
+	strcpy(ss,ct);
+	
+	struct dirent *d;
 	struct stat buf;
-	if(stat(s,&buf)==0) {
-		time = buf.st_ctime;
-		size = buf.st_size;
-		(*a)++;
-		if(size>=s1 && size<=s2 && time>=t1 && time<=t2) {
-			fputs(s,file);
-			fprintf(file,"\n");
-			fputs(ctime(&buf.st_ctime),file);
-			fprintf(file,"%d\n",size);
-		} 
+
+	strcat(s,con);
+	strcat(ss,con);
+
+	if(strcmp(con,"")!=0) strcat(ss,"/");
+	DIR *dr = opendir(ss);
+	
+	if(dr!=NULL) 
+	{
+		d = readdir(dr);
+		while (d != NULL && strcmp(d->d_name,".")!=0) 
+		{
+			(*a)++;
+			ReadCat(ss,d->d_name,t1,t2,s1,s2,file,a);
+			d = readdir(dr);
+		}
 	}
-}
+	else {
+		struct stat buf;
+		if(stat(s,&buf)==0) 
+		{
+			time = buf.st_ctime;
+			size = buf.st_size;
+			(*a)++;
+			if(size>=s1 && size<=s2 && time>=t1 && time<=t2) 
+			{
+				fputs(s,file);
+				fprintf(file,"\n");
+				fputs(ctime(&buf.st_ctime),file);
+				fprintf(file,"%d\n",size);
+			} 
+		}
+	}
 
-closedir(dr);
+	closedir(dr);
 
-return;
+	return;
 }	
 
 int main(int argc, char *argv[]) {
-char *path = argv[1];
-FILE *fl = fopen(argv[2],"w+t");
-int a = 0;
-int s1,s2,t1,t2;
-sscanf(argv[3],"%d",&t1);
-sscanf(argv[4],"%d",&t2);
-sscanf(argv[5],"%d",&s1);
-sscanf(argv[6],"%d",&s2);
-ReadCat(path,"",t1,t2,s1,s2,fl,&a);
-printf("Просмотренных файлов и каталогов = %d\n",a);
-fclose(fl);
+	char *path = argv[1];
+	FILE *fl = fopen(argv[2],"w+t");
+	int a = 0;
+	int s1,s2,t1,t2;
+	sscanf(argv[3],"%d",&t1);
+	sscanf(argv[4],"%d",&t2);
+	sscanf(argv[5],"%d",&s1);
+	sscanf(argv[6],"%d",&s2);
+	ReadCat(path,"",t1,t2,s1,s2,fl,&a);
+	printf("Просмотренных файлов и каталогов = %d\n",a);
+	fclose(fl);
 }
