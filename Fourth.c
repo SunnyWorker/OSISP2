80c4af801f07cbfd3e5c99fc5c57838cbe1b1816
SunnyWorker <93293748+SunnyWorker@users.noreply.github.com>
2022-03-25 09:16:26 +0300

    Update Fourth.c

--- Fourth.c
+++ Fourth.c
@@ -7,7 +7,10 @@ FILE *file;
 FILE *file2;
 file = fopen(argv[1],"r");
 file2 = fopen(argv[2],"w");
-
+if(file2==NULL) {
+printf("FILE NOT FOUND EXEPTION!\n");
+return 0;
+}
 struct stat buf;
 lstat(argv[1], &buf);
 chmod(argv[2], buf.st_mode);
@@ -16,11 +19,12 @@ chmod(argv[2], buf.st_mode);
 //if(access(argv[1],2) && access(argv[1],3)) chmod(argv[2],S_IREAD | S_IWRITE);
 char c = 0;
 while(!feof(file)) {
-	c = getc(file);
-	putc(c,file2);
+        c = getc(file);
+        putc(c,file2);
 }
 
 fclose(file);
 fclose(file2);
 
 }
+
