f4b1bbe2e4e8805579a0cef9f664de9876f58252
SunnyWorker <93293748+SunnyWorker@users.noreply.github.com>
2022-04-16 09:40:56 +0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 int main(int argc, char *argv[]) {
-if(argc<3) {
-        fprintf(stderr,"Not enough arguments!\n");
-        exit(EXIT_FAILURE);     
-}
+	if(argc<3) {
+		fprintf(stderr,"Not enough arguments!\n");
+		exit(EXIT_FAILURE);     
+	}
 
-FILE *file;
-FILE *file2;
-file = fopen(argv[1],"r");
-file2 = fopen(argv[2],"w");
-if(file==NULL) {
-	fprintf(stderr,"FILE NOT FOUND EXEPTION!\n");
-	return 1;
-}
-struct stat buf;
-lstat(argv[1], &buf);
-chmod(argv[2], buf.st_mode);
+	FILE *file;
+	FILE *file2;
+	file = fopen(argv[1],"r");
+	file2 = fopen(argv[2],"w");
+	if(file==NULL) {
+		fprintf(stderr,"FILE NOT FOUND EXEPTION!\n");
+		return 1;
+	}
+	struct stat buf;
+	lstat(argv[1], &buf);
+	chmod(argv[2], buf.st_mode);
 
-char c = 0;
-while(!feof(file)) {
-	c = getc(file);
-	putc(c,file2);
-}
+	char c = 0;
+	while(!feof(file)) {
+		c = getc(file);
+		putc(c,file2);
+	}
 
-fclose(file);
-fclose(file2);
+	fclose(file);
+	fclose(file2);
 
 }
