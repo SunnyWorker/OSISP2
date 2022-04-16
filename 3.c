437fc8ef45d2c97d57657294db4f0d98358bdc38
SunnyWorker <93293748+SunnyWorker@users.noreply.github.com>
2022-04-16 09:39:56 +0300

    Update 3.c

--- 3.c
+++ 3.c
@@ -3,44 +3,44 @@
 #include <stdlib.h>
 #include <errno.h>
 int main(int argc, char *argv[]) {
-if(argc<3) {
-        fprintf(stderr,"Not enough arguments!\n");
-        exit(EXIT_FAILURE);     
-}
-FILE *file;
-file = fopen(argv[1],"r");
-if(file==NULL) {
-fprintf(stderr,"Not found file exception!\n");
-exit(EXIT_FAILURE);
-}
-char c = 0;
-int k = 0,a = 0,i = 0;
-while(argv[2][i]!=0) {
-	if(argv[2][i]<'0' || argv[2][i]>'9') {
-		fprintf(stderr,"Wrong input!\n");
+	if(argc<3) {
+		fprintf(stderr,"Not enough arguments!\n");
+		exit(EXIT_FAILURE);     
+	}
+	FILE *file;
+	file = fopen(argv[1],"r");
+	if(file==NULL) {
+		fprintf(stderr,"Not found file exception!\n");
 		exit(EXIT_FAILURE);
 	}
-	else {
-		k*=10;
-		k+=argv[2][i]-48;
-		if(k<0) {
-			fprintf(stderr,"Error. Too big number!\n");
+	char c = 0;
+	int k = 0,a = 0,i = 0;
+	while(argv[2][i]!=0) {
+		if(argv[2][i]<'0' || argv[2][i]>'9') {
+			fprintf(stderr,"Wrong input!\n");
 			exit(EXIT_FAILURE);
 		}
+		else {
+			k*=10;
+			k+=argv[2][i]-48;
+			if(k<0) {
+				fprintf(stderr,"Error. Too big number!\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		i++;
 	}
-	i++;
-}
 
-while(!feof(file)) {
-	c = getc(file);
-	if(c==10) a++;
-	if(a==k && a!=0) {
-		printf("\nClick ENTER to continue");
-		getchar();
-		a=0;
+	while(!feof(file)) {
+		c = getc(file);
+		if(c==10) a++;
+		if(a==k && a!=0) {
+			printf("\nClick ENTER to continue");
+			getchar();
+			a=0;
+		}
+		else printf("%c",c);
 	}
-	else printf("%c",c);
-}
-fclose(file);
+	fclose(file);
 
 }
